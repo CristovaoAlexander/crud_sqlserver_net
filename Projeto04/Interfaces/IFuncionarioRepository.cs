be6a69bfc3f0ca51be87caf1b2d5ca15b0363ae0
CristovaoAlexander <alex.cafr.fac@gmail.com>
2023-05-08 21:48:54 -0300

    first commit

---
+++ Projeto04/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using Projeto04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04.Interfaces
+{
+    public interface IFuncionarioRepository : IBaseRepository<Funcionario>
+    {   // os metodos passam a vir da interface generica IBase
+        //void Add(Funcionario funcionario);
+        //void Update(Funcionario funcionario);
+        //void Delete(Funcionario funcionario);
+        //List<Funcionario> GetAll();
+        //Funcionario? GetById(Guid idFuncionario);
+    }
+}
